4bbee80aca97a48dc432d2695a24c314d78b9b57
Yash Jain <yash@Yashs-MacBook-Air.local>
2024-01-04 00:50:47 +0530

    add changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import ArchiveToggle from './components/ArchiveToggle/ArchiveToggle.jsx';
 const Header = ({ showarchive, setShowArchive }) => {
   return (
     <header>
-      <svg width='486px' height='168px' viewBox='0 0 486 168' version='1.1' xmlns='http://www.w3.org/2000/svg'>
+      <svg width='100px' height='100px' viewBox='0 0 486 168' version='1.1' xmlns='http://www.w3.org/2000/svg'>
         <g stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
           <g transform='translate(207.000000, 24.000000)'>
             <rect fill='#424242' x='270' y='6' width='9' height='90' rx='4.5' />
